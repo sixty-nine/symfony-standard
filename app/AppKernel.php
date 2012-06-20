2aabf4f4577ad91a16c0eafb700c14bad4a83980
dan <dan@dreamcraft.ch>
2012-06-20 09:45:56 +0200

    Added imagine demo bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new \Liip\ImagineBundle\LiipImagineBundle(),
+            new Liip\ImagineDemoBundle\LiipImagineDemoBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
